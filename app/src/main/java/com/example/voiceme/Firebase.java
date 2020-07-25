43c20629af9be702dbfff8207d4ff046df5cd100
elsakrismonti <elsakrismonti123@gmail.com>
2020-07-25 17:52:54 +0700

    Kelapa Muda

--- app/src/main/java/com/example/voiceme/Firebase.java
+++ app/src/main/java/com/example/voiceme/Firebase.java
@@ -2,9 +2,18 @@ package com.example.voiceme;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Firebase {
     public static FirebaseAuth auth(){return FirebaseAuth.getInstance();}
     public static FirebaseUser currenntUser(){return auth().getCurrentUser();}
+    public static class DataBase{
+        static FirebaseDatabase db(){return FirebaseDatabase.getInstance();}
+        public static DatabaseReference user(){return db().getReference("users");}
+        public static DatabaseReference user(String phoneNumber){return user().child(phoneNumber);}
+    }
 
 }
+
